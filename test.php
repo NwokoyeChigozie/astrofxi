d103c466bf13b755303f018b8e35fda0c4348cca
Greg <gregoflash05@gmail.com>
2021-12-02 01:21:49 -0500

    first

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+include('phpscripts/connection.php');
+//$password = "cryptoservice";
+$password = "Hyman@1992";
+$password = hash('sha256', $password);
+echo "$password";
+//value = 977302936afc03b7637c7e0acd6273fd80a9898e01733251fa589d058206ee88;
+$username="astroadmin";
+?>
