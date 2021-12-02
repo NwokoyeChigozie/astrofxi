d8403d8118b7ac7c42b8cc8e7a88c9b82760a638
Greg <gregoflash05@gmail.com>
2021-12-02 01:32:17 -0500

    first

---
+++ phpscripts/connection.php
@@ -0,0 +1,8 @@
+<?php
+//sql connection
+$link = mysqli_connect("localhost", "astrljco_admin", "cryptoservice", "astrljco_astro");
+if(mysqli_connect_error()){
+    die('ERROR: Unable to connect:' . mysqli_connect_error());
+}
+
+    ?>
